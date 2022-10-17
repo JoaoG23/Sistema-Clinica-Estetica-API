eb388bee7a3b6858ae3a9f38b6045f0f9fcab2e4
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2022-10-17 09:42:44 -0300

    Criando Teste Automatizados

--- src/__test__/integrations/seedsForTests/userExample.ts
+++ src/__test__/integrations/seedsForTests/userExample.ts
@@ -5,6 +5,19 @@ export const angela = {
     idPrevilegies: 1
   
 };
+export const emailAngelaAndNewPassword = {
+    password:"angela",
+    email:"angela@angela.com",
+  
+};
+export const gabiroba = {
+    userName:"gabiroba",
+    password:"gabiroba",
+    email:"gabiroba@gabiroba.com",
+    idPrevilegies: 1
+  
+};
+
 export const angelaLogin = {
   userName: "angela",
   password: "angela",
